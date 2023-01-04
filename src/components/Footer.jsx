a466c8b15867ad3ca8db702cd080075e198e8d60
dasimems <isaacseun63@gmail.com>
2023-01-04 19:46:52 +0100

    did the footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Row, Col } from "antd"
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+
+        <Row justify="center" className="footer">
+            <Col span={22} className="footer-container">
+                <Row justify="space-between" className='footer-content'>
+                    <ul className='social-links'></ul>
+
+                    <p className="copyright">Developed By Dasimems &copy; YouStudio 2022</p>
+                </Row>
+            </Col>
+
+        </Row>
+    
+    </>
   )
 }
 
